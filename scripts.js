00c775856dc82488c4ba01d00d04211ccd98a33f
RonaldoDomingossp <134653907+RonaldoDomingossp@users.noreply.github.com>
2023-05-28 13:50:59 -0300

    previsão do tempo hoje
    adicionei no meu projeto imagem, html, css e Java scripts

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+
+const key = "79509d5e76a4a9261a20c146adf84065"
+
+function colocardadosnatela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+
+}
+
+async function buscarcidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+
+    colocardadosnatela(dados)
+
+}
+
+
+
+
+function cliqueinobotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarcidade(cidade)
+
+}
